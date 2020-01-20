347a9051f9507eceb538f72779a488ff199bca63
Max Gaukler <development@maxgaukler.de>
2020-01-20 21:44:19 +0100

    Thunderlaser: deduplicate estimateJobDuration

--- src/main/java/de/thomas_oster/liblasercut/drivers/ThunderLaser.java
+++ src/main/java/de/thomas_oster/liblasercut/drivers/ThunderLaser.java
@@ -159,9 +159,6 @@ public class ThunderLaser extends LaserCutter
   public void setAddSpacePerRasterLine(double addSpacePerRasterLine) {
     this.addSpacePerRasterLine = addSpacePerRasterLine;
   }
-  /*
-   * estimateJobDuration - copied from EpilogCutter
-   */
 
   @Override
   public int estimateJobDuration(LaserJob job)
@@ -174,110 +171,9 @@ public class ThunderLaser extends LaserCutter
     //TODO: The Raster3d values are not tested yet, theyre just copies
     double RASTER3D_LINEOFFSET = 0.08;
     double RASTER3D_LINESPEED = 100000d / ((268d / 50) - RASTER3D_LINEOFFSET);
-
-    //Holds the current Laser Head position in Pixels
-    Point p = new Point(0, 0);
-
-    double result = 0;//usual offset
-    for (JobPart jp : job.getParts())
-    {
-      if (jp instanceof RasterPart)
-      {
-        RasterPart rp = (RasterPart) jp;
-        Point sp = rp.getRasterStart();
-        result += Math.max((double) (p.x - sp.x) / VECTOR_MOVESPEED_X,
-          (double) (p.y - sp.y) / VECTOR_MOVESPEED_Y);
-        double linespeed = ((double) RASTER_LINESPEED * ((ThunderLaserProperty) rp.getLaserProperty()).getSpeed()) / 100;
-        ByteArrayList line = new ByteArrayList(rp.getRasterWidth());
-        for (int y = 0; y < rp.getRasterHeight(); y++)
-        {//Find any black point
-          boolean lineEmpty = true;
-	  rp.getRasterLine(y, line);
-          for (byte b : line)
-          {
-            if (b != 0)
-            {
-              lineEmpty = false;
-              break;
-            }
-          }
-          if (!lineEmpty)
-          {
-            int w = rp.getRasterWidth();
-            result += (double) RASTER_LINEOFFSET + (double) w / linespeed;
-            p.x = sp.y % 2 == 0 ? sp.x + w : sp.x;
-            p.y = sp.y + y;
-          }
-          else
-          {
-            result += RASTER_LINEOFFSET;
-          }
-        }
-      }
-      if (jp instanceof Raster3dPart)
-      {
-        Raster3dPart rp = (Raster3dPart) jp;
-        Point sp = rp.getRasterStart();
-        result += Math.max((double) (p.x - sp.x) / VECTOR_MOVESPEED_X,
-          (double) (p.y - sp.y) / VECTOR_MOVESPEED_Y);
-        double linespeed = ((double) RASTER3D_LINESPEED * ((ThunderLaserProperty) rp.getLaserProperty()).getSpeed()) / 100;
-	ByteArrayList line = new ByteArrayList(rp.getRasterWidth());
-        for (int y = 0; y < rp.getRasterHeight(); y++)
-        {//Check if
-          boolean lineEmpty = true;
-	  rp.getRasterLine(y, line);
-          for (byte b : line)
-          {
-            if (b != 0)
-            {
-              lineEmpty = false;
-              break;
-            }
-          }
-          if (!lineEmpty)
-          {
-            int w = rp.getRasterWidth();
-            result += (double) RASTER3D_LINEOFFSET + (double) w / linespeed;
-            p.x = sp.y % 2 == 0 ? sp.x + w : sp.x;
-            p.y = sp.y + y;
-          }
-        }
-      }
-      if (jp instanceof VectorPart)
-      {
-        double speed = VECTOR_LINESPEED;
-        VectorPart vp = (VectorPart) jp;
-        for (VectorCommand cmd : vp.getCommandList())
-        {
-          switch (cmd.getType())
-          {
-            case SETPROPERTY:
-            {
-              speed = VECTOR_LINESPEED * ((ThunderLaserProperty) cmd.getProperty()).getSpeed() / 100;
-              break;
-            }
-            case MOVETO:
-              result += Math.max((double) (p.x - cmd.getX()) / VECTOR_MOVESPEED_X,
-                (double) (p.y - cmd.getY()) / VECTOR_MOVESPEED_Y);
-              p = new Point(cmd.getX(), cmd.getY());
-              break;
-            case LINETO:
-              double dist = distance(cmd.getX(), cmd.getY(), p);
-              p = new Point(cmd.getX(), cmd.getY());
-              result += dist / speed;
-              break;
-          }
-        }
-      }
-    }
-    return (int) result;
+   
+    return estimateJobDuration(job, VECTOR_MOVESPEED_X, VECTOR_MOVESPEED_Y, VECTOR_LINESPEED, RASTER_LINEOFFSET, RASTER_LINESPEED, RASTER3D_LINEOFFSET, RASTER3D_LINESPEED);
   }
-
-  private double distance(double x, double y, Point p)
-  {
-    return Math.sqrt(Math.pow(p.x - x, 2) + Math.pow(p.y - y, 2));
-  }
-
 
   /*
    * checkJob - copied from EpilogCutter
